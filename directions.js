937922c8f8dfc88d72121830b39efe4654a3d6c7
yang4214-hash <yang4214@gmail.com>
2026-06-09 23:48:46 +0900

    Update directions.js

--- directions.js
+++ directions.js
@@ -1,32 +1,24 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  const { start, goal } = req.query;
-  if (!start || !goal) {
-    return res.status(400).json({ error: 'start, goal 파라미터 필요' });
+  const { origin, destination } = req.query;
+  if (!origin || !destination) {
+    return res.status(400).json({ error: 'origin, destination 파라미터 필요' });
   }
 
-  const clientId = process.env.NAVER_CLIENT_ID;
-  const clientSecret = process.env.NAVER_CLIENT_SECRET;
-
-  if (!clientId || !clientSecret) {
-    return res.status(500).json({ error: 'API 키가 설정되지 않았습니다.' });
+  const appKey = process.env.KAKAO_REST_API_KEY;
+  if (!appKey) {
+    return res.status(500).json({ error: 'API 키 없음' });
   }
 
   try {
-    const url = `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${start}&goal=${goal}&option=traoptimal`;
-
+    const url = `https://apis-navi.kakaomobility.com/v1/directions?origin=${origin}&destination=${destination}&priority=RECOMMEND`;
     const response = await fetch(url, {
-      headers: {
-        'X-NCP-APIGW-API-KEY-ID': clientId,
-        'X-NCP-APIGW-API-KEY': clientSecret,
-      }
+      headers: { 'Authorization': `KakaoAK ${appKey}` }
     });
-
     const data = await response.json();
     return res.status(200).json(data);
-
-  } catch (e) {
+  } catch(e) {
     return res.status(500).json({ error: e.message });
   }
 }
